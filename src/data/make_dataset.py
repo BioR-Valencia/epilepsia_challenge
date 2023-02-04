4a5aa1489d2382498bd5a17ed2a76cb518825084
jmjaenlorites <josemanueljaenlorites@gmail.com>
2023-02-04 20:25:42 +0100

    modify get generator function to return train and test generator and generator also returns filepath

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,7 @@ import os
 from abc import abstractmethod
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def example_function():
@@ -44,17 +45,59 @@ class DataLoader:
         )
         return dataset
 
-    def get_generator(self, patient_id: int):
-        def generator():
-            patient_labels = self._get_patient_labels(
-                patient_id, skip_missing_files=True
-            )
-            for patient_file, label in patient_labels.values:
+    def get_generators(self, patient_id: int):
+        patient_labels = self._get_patient_labels(patient_id, skip_missing_files=True)
+        train_labels, test_labels = self._split_dataset_labels(patient_labels)
+
+        def train_generator():
+            for patient_file, label in train_labels.values:
                 yield self._parquet_reader_function(
                     os.path.join(self.dataset_path, patient_file)
-                ), label
+                ), label, patient_file
 
-        return generator()
+        def test_generator():
+            for patient_file, label in test_labels.values:
+                yield self._parquet_reader_function(
+                    os.path.join(self.dataset_path, patient_file)
+                ), label, patient_file
+
+        return train_generator(), test_generator()
+
+    def _split_dataset_labels(self, labels: pd.DataFrame):
+        labels = self._filter_folders_without_min_files(labels)
+        labels = self._prune_labels(labels)
+
+        # Split labels in train and test with sklearn
+        train_labels, test_labels = train_test_split(
+            labels, test_size=0.2, random_state=42, stratify=labels["label"]
+        )
+        return train_labels, test_labels
+
+    @staticmethod
+    def _prune_labels(labels: pd.DataFrame):
+        labels["sum_next_5_labels"] = 0
+        for i in range(1, 6):
+            labels["sum_next_5_labels"] += labels["label"].shift(-i)
+
+        labels_near_to_1 = labels[labels["sum_next_5_labels"] > 0]
+
+        labels_random_0 = labels[
+            (labels["label"] == 0) & (labels["sum_next_5_labels"] == 0)
+        ].sample(int(len(labels_near_to_1) / 6), random_state=42)
+
+        final_labels = pd.concat([labels_near_to_1, labels_random_0])
+        final_labels.drop(columns=["sum_next_5_labels"], inplace=True)
+        return final_labels
+
+    @staticmethod
+    def _filter_folders_without_min_files(labels: pd.DataFrame, min_files: int = 6):
+        labels["patient"] = labels["filepath"].apply(lambda x: x[:4])
+        labels["folder"] = labels["filepath"].apply(lambda x: x[5:8])
+        grouped_labels = labels.groupby(by=["folder"]).count()
+        mask = grouped_labels[grouped_labels["filepath"] > min_files]
+        labels = labels[labels["folder"].isin(mask.index)]
+        labels.drop(columns=["patient", "folder"], inplace=True)
+        return labels
 
     def _get_patient_labels(self, patient_id: int, skip_missing_files: bool):
         patient_labels = self._labels[
